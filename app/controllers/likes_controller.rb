48e6f870e934dd8cdfff57eba09aa1ca5fffaff6
rakave <velu.ravichandran@chicagobooth.edu>
2015-11-22 20:37:03 -0600

    Completed!!!! Booyah!

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,7 +17,7 @@ class LikesController < ApplicationController
     @like.photo_id = params[:photo_id]
 
     if @like.save
-      redirect_to "/likes", :notice => "Like created successfully."
+      redirect_to :back, :notice => "Like created successfully."
     else
       render 'new'
     end
@@ -50,6 +50,6 @@ class LikesController < ApplicationController
 
     @like.destroy
 
-    redirect_to "/likes", :notice => "Like deleted."
+    redirect_to :back, :notice => "Like deleted."
   end
 end
